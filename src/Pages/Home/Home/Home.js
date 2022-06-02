f209605f010931697a22b3360c9a5a03ea7d9c00
shoronshaha <smaranshaha@gmail.com>
2022-06-02 16:34:46 +0600

    added requireauth and usetool hooks

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
+import Tools from '../ToolProduct/Tools';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+
         </div>
     );
 };
