4e7ff4c86b8e23aed6c5aa90924552955cce8c28
shoronshaha <smaranshaha@gmail.com>
2022-06-02 15:28:07 +0600

    added footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
+import Footer from './Pages/Shared/Footer';
 import Navbar from './Pages/Shared/Navbar';
 
 function App() {
   return (
     <div >
       <Navbar></Navbar>
+      <Footer></Footer>
     </div>
   );
 }
